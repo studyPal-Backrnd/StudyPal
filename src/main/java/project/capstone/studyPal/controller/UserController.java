e763fc98a25ccfb83af6095882a7029e3556d92b
David <osodavid001@gmail.com>
2023-05-31 14:07:08 +0100

    validate user controller

--- src/main/java/project/capstone/studyPal/controller/UserController.java
+++ src/main/java/project/capstone/studyPal/controller/UserController.java
@@ -1,5 +1,6 @@
 package project.capstone.studyPal.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,7 +21,7 @@ public class UserController {
  private final UserService userService;
 
  @PostMapping("registerUser")
- public ResponseEntity<?> registerUser(@RequestBody UserRegisterRequest userDto) {
+ public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegisterRequest userDto) {
   userService.register(userDto);
   return ResponseEntity.status(HttpStatus.CREATED).body("check mail for verification code");
  }
@@ -31,24 +32,24 @@ public class UserController {
 //  return ResponseEntity.ok(response);
 // }
 @GetMapping("verify")
- public ResponseEntity <?> verifyAccount(@RequestParam String email, String verificationToken) {
+ public ResponseEntity <?> verifyAccount(@Valid @RequestParam String email, String verificationToken) {
  UserResponse response = userService.verifyAccount(email, verificationToken);
  return ResponseEntity.ok(response);
 }
 
 @GetMapping("get")
- public ResponseEntity<?> getUserByEmail(@RequestParam String email){
+ public ResponseEntity<?> getUserByEmail(@Valid @RequestParam String email){
   AppUser foundUser = userService.getUserByEmail(email);
   return ResponseEntity.status(HttpStatus.OK).body(foundUser);
 }
 @GetMapping("{userId}")
- public ResponseEntity<?> getUserById(@PathVariable Long userId){
+ public ResponseEntity<?> getUserById(@Valid @PathVariable Long userId){
   AppUser foundUser = userService.getUserById(userId);
   return ResponseEntity.status(HttpStatus.OK).body(foundUser);
 }
 
 @PostMapping("login")
- public ResponseEntity <UserResponse> loginToAccount(@RequestParam String email, @RequestParam String password) {
+ public ResponseEntity <UserResponse> loginToAccount(@Valid @RequestParam String email, @Valid @RequestParam String password) {
   UserResponse response = userService.login(email, password);
  return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
  }
